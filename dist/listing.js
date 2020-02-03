e625c6c7ca3378b2b76a75a46bacb1fcaf34d0d6
Dino Pascale <dino.pascale@onedaygroup.it>
2020-02-03 18:02:39 +0100

    removed dist folder and added to gitignore

--- dist/listing.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = require("fs");
-var os_1 = require("os");
-function ls(path) {
-    return fs_1.promises.readdir(path || os_1.homedir());
-}
-exports.default = ls;
